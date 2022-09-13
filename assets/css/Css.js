0d6d52696806ae7eb8cff8d4a11c73ab022d8f27
Marcos Capiberibe <marcoscapiberibe@hotmail.com>
2022-09-13 04:40:05 -0300

    Criação e estilização do componente Map
    Criação e estilização do componente Map

---
+++ assets/css/Css.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+const css = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        // alignItems: 'center',
+        justifyContent: 'center',
+    },
+    topo: {
+        height: '10%',
+        backgroundColor: 'red',
+    },
+    map: {
+        width: '100%',
+        height: '72%',
+        backgroundColor: 'black',
+    },
+    iniciar: {
+        height: '18%',
+        backgroundColor: 'gray',
+        color: 'black',
+    },
+});
+
+export { css };
